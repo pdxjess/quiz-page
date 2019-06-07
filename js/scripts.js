6ef5c5c45b1a6874f92293714c03d16bec568af3
Jessica Doss <doss.jessicam@gmail.com>
2019-06-07 11:11:19 -0700

    add more images and create variables for js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document)ready(function(){
+  var question1 = parseInt($("input:radio[name=favMusic]:checked").val());
+  var question2 = parseInt($("input:radio[name=favVac]:checked").val());
+  var question3 = parseInt($("input:radio[name=favColors]:checked").val());
+  var question4 = parseInt($("input:radio[name=favHouse]:checked").val());
+  var question5 = parseInt($("input:radio[name=favGuest]:checked").val());
 
+  var total = question1 + question2 + question3 + question4 + question5;
 });
