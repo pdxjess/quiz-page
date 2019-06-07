29fda1c41e2e286ef8eca44fd3bf48e73df1eb5d
Jessica Doss <doss.jessicam@gmail.com>
2019-06-07 12:04:17 -0700

    fix js function to display result

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
-$(document)ready(function(){
-  var question1 = parseInt($("input:radio[name=favMusic]:checked").val());
-  var question2 = parseInt($("input:radio[name=favVac]:checked").val());
-  var question3 = parseInt($("input:radio[name=favColors]:checked").val());
-  var question4 = parseInt($("input:radio[name=favHouse]:checked").val());
-  var question5 = parseInt($("input:radio[name=favGuest]:checked").val());
+$(document).ready(function(){
+  $("form").submit(function(event){
 
-  var total = question1 + question2 + question3 + question4 + question5;
+    var question1 = parseInt($("input:radio[name=favMusic]:checked").val());
+    var question2 = parseInt($("input:radio[name=favVac]:checked").val());
+    var question3 = parseInt($("input:radio[name=favColors]:checked").val());
+    var question4 = parseInt($("input:radio[name=favHouse]:checked").val());
+    var question5 = parseInt($("input:radio[name=favGuest]:checked").val());
 
-  if (total <=3) {
-    $(".javascript").show();
-  } else if (total <=5) {
-    $(".csharp").show();
-  } else {
-    $(".react").show();
-  }
-  event.preventDefault();
+    var total = question1 + question2 + question3 + question4 + question5;
+
+    if (total <=3) {
+      $(".ruby").show();
+    } else if (total <=5) {
+      $(".csharp").show();
+    } else {
+      $(".react").show();
+    }
+    event.preventDefault();
+  });
 });
